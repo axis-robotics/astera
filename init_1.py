7b9007604ab25ab9badf0964b33570223be52e96
Astera <neatphar@gmail.com>
2020-12-29 03:51:38 +0200

    preperation for i2c

--- init_1.py
+++ init_1.py
@@ -1,20 +1,20 @@
-import smbus
+import smbus2
 import json
 import Jetson.GPIO as GPIO
 import base
 
-bus = smbus.SMBus(1)
+bus = smbus2.SMBus(1)
 ADDRESS = 0x04
 SWITCH_INPUT_PIN = 6
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(SWITCH_INPUT_PIN, GPIO.IN)
 
 def write_data_to_avr(value):
-    byteValue = [ord(c) for c in value]
-    return bus.write_i2c_block_data(ADDRESS, 0x00, byteValue)
+    return bus.write_byte(ADDRESS, value)
+    # byteValue = [ord(c) for c in value]
+    # return bus.write_i2c_block_data(ADDRESS, 0x00, byteValue)
 
 def read_data_from_avr():
-    # TODO: test...
     number = bus.read_byte(ADDRESS)
     # number = bus.read_byte_data(ADDRESS, 1)
     return number
