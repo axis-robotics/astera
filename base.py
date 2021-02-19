987bd3f44b4c76c98c7a2031444333eea11bbd69
neatphar <neatphar@gmail.com>
2021-02-19 13:54:48 +0200

    removed useless code

--- base.py
+++ base.py
@@ -3,11 +3,87 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # Supress TF debugging info. (Uncomment
 
 import math
 import cv2
+import colorsys, random
 import numpy as np
 import tensorflow as tf
 
-import core.utils as utils
-from core.yolov4 import filter_boxes
+def read_class_names(class_file_name):
+    names = {}
+    with open(class_file_name, 'r') as data:
+        for ID, name in enumerate(data):
+            names[ID] = name.strip('\n')
+    return names
+
+def detect_coordinates(image, bboxes, classes=read_class_names("./weights/classes.names"), show_label=True):
+    num_classes = len(classes)
+    image_h, image_w, _ = image.shape
+    hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+
+    random.seed(0)
+    random.shuffle(colors)
+    random.seed(None)
+
+    out_boxes, out_scores, out_classes, num_boxes = bboxes
+    return_value = []
+    for i in range(num_boxes[0]):
+        if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > num_classes: continue
+        coor = out_boxes[0][i]
+        coor[0] = int(coor[0] * image_h)
+        coor[2] = int(coor[2] * image_h)
+        coor[1] = int(coor[1] * image_w)
+        coor[3] = int(coor[3] * image_w)
+
+        fontScale = 0.5
+        score = out_scores[0][i]
+        class_ind = int(out_classes[0][i])
+        c1, c2 = (int(coor[1]), int(coor[0])), (int(coor[3]), int(coor[2]))
+        return_value.append(
+            {
+                "image": image[c1[1]:c2[1], c1[0]:c2[0]],
+                "center": ((c1[0]+c2[0]) // 2, (c1[1]+c2[1]) // 2)
+            }
+        )
+    return return_value
+
+
+
+
+
+
+def filter_boxes(box_xywh, scores, score_threshold=0.4, input_shape = tf.constant([416,416])):
+    scores_max = tf.math.reduce_max(scores, axis=-1)
+
+    mask = scores_max >= score_threshold
+    class_boxes = tf.boolean_mask(box_xywh, mask)
+    pred_conf = tf.boolean_mask(scores, mask)
+    class_boxes = tf.reshape(class_boxes, [tf.shape(scores)[0], -1, tf.shape(class_boxes)[-1]])
+    pred_conf = tf.reshape(pred_conf, [tf.shape(scores)[0], -1, tf.shape(pred_conf)[-1]])
+
+    box_xy, box_wh = tf.split(class_boxes, (2, 2), axis=-1)
+
+    input_shape = tf.cast(input_shape, dtype=tf.float32)
+
+    box_yx = box_xy[..., ::-1]
+    box_hw = box_wh[..., ::-1]
+
+    box_mins = (box_yx - (box_hw / 2.)) / input_shape
+    box_maxes = (box_yx + (box_hw / 2.)) / input_shape
+    boxes = tf.concat([
+        box_mins[..., 0:1],  # y_min
+        box_mins[..., 1:2],  # x_min
+        box_maxes[..., 0:1],  # y_max
+        box_maxes[..., 1:2]  # x_max
+    ], axis=-1)
+    # return tf.concat([boxes, pred_conf], axis=-1)
+    return (boxes, pred_conf)
+
+
+
+
+
+
 
 from tensorflow.python.saved_model import tag_constants
 from tensorflow.compat.v1 import ConfigProto
@@ -68,7 +144,7 @@ def detect_flowers(original_image):
     )
 
     pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
-    return utils.detect_coordinates(original_image, pred_bbox)
+    return detect_coordinates(original_image, pred_bbox)
 
 # Classification model.
 
@@ -133,3 +209,17 @@ def transformation_matrix(flower):
     return [
         ((i[0] + FLAGS.ANGLE_SHIFT) * FLAGS.ANGLE_SCALE if np.abs(i[0]) < np.abs(i[1]) else i[1]) for i in t
     ]
+
+
+
+for i in range(1, 3):
+    img = preprocess_cam(f"examples/{i}.jpg")
+    flowers = detect_flowers(img)
+    y = classify_flowers(flowers)
+    original_image = cv2.imread(f"examples/{i}.jpg")
+    print(len(flowers))
+    for j in flowers:
+        original_image = cv2.circle(original_image, j['center'], 1, (0, 0, 255), 5)
+    cv2.imwrite(f"examples/done - {i}.jpg", original_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
